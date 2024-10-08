30bf8905124c634efcf1bb6d7cea8ae63ce564a2
MHJedli <mouadjadli10@gmail.com>
2024-10-08 20:22:59 +0100

    Add NVIDIA Driver Installer entry to System Tweaks Menu

--- src/menus/tweaks.sh
+++ src/menus/tweaks.sh
@@ -13,27 +13,34 @@ showSystemTweaksMenu(){
     "Install PipeWire Sound System" \
     "Fix Keyboard RGB backlight" \
     "Return to Previous Menu"
+    
     echo -n "Enter Option : "
     read option
     log_message "INFO" "User selected option $option in System Tweaks Menu"
 
     case $option in
         1)
-            log_message "INFO" "User chose to Install PyTorch"
+            log_message "INFO" "User chose to Install NVIDIA Driver"
+            log_message "INFO" "Displaying NVIDIA Driver Menu"
+            distroMenu "              NVIDIA Driver" \
+                       "${scriptPaths["nvidia_driver_installer"]}" \
+                       "showSystemTweaksMenu" \
             ;;
         2)
-            log_message "INFO" "User chose to Install Conda"
+            log_message "INFO" "User chose to Install PipeWire Sound System"
+            # showPipeWireMenu
             ;;
         3)
-            log_message "INFO" "User chose To Install TensorFlow"
+            log_message "INFO" "User chose To Fix Keyboard RGB Backlight"
+            # showKeyboardFixMenu
             ;;
         4)
             log_message "INFO" "User chose to Return to Previous Menu"
-            showMainMenu
+            # showMainMenu
             ;;
         *)
             log_message "WARN" "User chose an invalid option : $option"
-            invalidOption showAIMenu
+            invalidOption showSystemTweaksMenu
             ;;
     esac
 }
