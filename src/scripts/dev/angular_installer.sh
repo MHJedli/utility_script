8346446d353e4ea8a320f9a2bf050b1357e00deb
MHJedli <mouadjadli10@gmail.com>
2024-11-15 20:10:50 +0100

    Style : Update Message Colors in Angular Script Installer

--- src/scripts/dev/angular_installer.sh
+++ src/scripts/dev/angular_installer.sh
@@ -14,73 +14,80 @@ installAngular(){
         ["15"]=1
     )
     while true; do
-        echo -n "Install Latest Angular Version ? (Y/n) : "
+        echo -n -e "${CYAN}Install Latest Angular Version ? (Y/n) :${RESET} "
         read a
         log_message "INFO" "User chose option $a"
         if [[ "$a" == "Y" || "$a" == "y" || "$a" == ""  ]]; then
+
             log_message "INFO" "Installing Latest Angular Version"
-            echo "Installing Latest Angular Version..."
+            echo -e "${YELLOW}-> Installing Latest Angular Version...${RESET}"
             sleep 1
             npm install @angular/cli --location=global || handle_error "Failed to Install Latest Angular Version"
             return
+
         elif [[ "$a" == "n" || "$a" == "N" ]]; then
+
             log_message "INFO" "User chose to select specific Angular version"
-            echo "What Version Do you want to install ? (17, 16, 15)"
+            echo -e "What Version Do you want to install ? ${CYAN}(17, 16, 15)${RESET}"
             echo "Angular Version : "
             read version
             log_message "INFO" "User chose version $version"
             if [[ -v versions["$version"] ]]; then
+
                 log_message "INFO" "Installing Angular version $version"
-                echo "Installing Angular version $version"
+                echo -e "${YELLOW}Installing Angular version $version${RESET}"
                 sleep 1
                 npm install -g @angular/cli@"$version" || handle_error "Failed to Install Angular version $version"
                 return
+
             else
-                echo "Wrong Version Selected !"
+
+                echo -e "${RED}Wrong Version Selected !${RESET}"
                 read
                 clear
+
             fi
         fi
     done
 }
 
-echo "-> Checking for Internet Connection..."
+echo -e "${YELLOW}-> Checking for Internet Connection...${RESET}"
 sleep 1
 
 if check_internet; then
 
     log_message "INFO" "Internet Connection Detected. Proceeding with Angular Installation"
-    echo "-> Internet Connection Detected. Proceeding with Angular Installation..."
+    echo -e "${GREEN}-> Internet Connection Detected. Proceeding with Angular Installation...${RESET}"
 
     log_message "INFO" "Installing NVM"
-    echo "-> Installing NVM..."
+    echo -e "${YELLOW}-> Installing NVM...${RESET}"
     sleep 1
     curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash || handle_error "Failed to install NVM"
 
     log_message "INFO" "Activating NVM Environment"
-    echo "-> Activating NVM Environment..."
+    echo -e "${YELLOW}-> Activating NVM Environment...${RESET}"
     sleep 1
     source $HOME/.bashrc || handle_error "Failed to Activate NVM Environment" 
     source ~/.nvm/nvm.sh || handle_error "Failed to Activate NVM Environment"
     source ~/.profile    || handle_error "Failed to Activate NVM Environment"
 
     log_message "INFO" "Installing Node.js 18 LTS"
-    echo "-> Installing Node.js 18 LTS..."
+    echo -e "${YELLOW}-> Installing Node.js 18 LTS...${RESET}"
     sleep 1
     nvm install 18 || handle_error "Failed to install Node.js 18 LTS"
 
     log_message "INFO" "Checking Installed Node and NPM Versions"
-    echo "-> Checking Installed Node and NPM Versions..."
+    echo -e "${YELLOW}-> Checking Installed Node and NPM Versions...${RESET}"
     sleep 1
     node -v || handle_error "Failed to Print Installed Node Version"
     npm -v || handle_error "Failed to Print Installed NPM Version"
 
     log_message "INFO" "Installing Angular"
-    echo "-> Installing Angular..."
+    echo -e "${YELLOW}-> Installing Angular...${RESET}"
     sleep 1
     installAngular
 
-    echo -n "Angular Installed Successfully. Want to check your Installed Version ? (Y/n) : "
+    echo -n -e "${GREEN}Angular Installed Successfully .${RESET} Want to check your Installed Version ? (Y/n) : "
     read a
     if [[ "$a" == "y" || "$a" == "Y" || "$a" == "" ]];then
         log_message "INFO" "Printing Installed Angular Version"
@@ -88,6 +95,8 @@ if check_internet; then
     fi
 
     echo "Script Execution in Angular Installation Ended Successfully at $(date)" >> "$LOG_FILE"
+    echo "Press [ENTER] To Exit..."
+    read
 
 else
 
