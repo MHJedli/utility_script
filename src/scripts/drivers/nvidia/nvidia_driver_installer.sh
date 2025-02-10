70ff4980a49b37fb091d54bc062c9dd2034c0716
MHJedli <mouadjadli10@gmail.com>
2025-02-10 14:59:33 +0100

    add: nvidia driver version 570

--- src/scripts/drivers/nvidia/nvidia_driver_installer.sh
+++ src/scripts/drivers/nvidia/nvidia_driver_installer.sh
@@ -13,7 +13,8 @@ download_and_install(){
 install_driver(){
     log_message "INFO" "Displaying NVIDIA Driver Menu"
     local driver_options=$(whiptail --title "NVIDIA Driver Installer" --menu "Choose a Driver Version" 30 80 10 \
-    "NVIDIA Driver Version 560 [Latest Driver Currently]" "" \
+    "NVIDIA Driver Version 570 [Latest Driver Currently]" "" \
+    "NVIDIA Driver Version 560" "" \
     "NVIDIA Driver Version 550" "" \
     "NVIDIA Driver Version 545" "" \
     "NVIDIA Driver Version 535" "" \
@@ -28,7 +29,10 @@ install_driver(){
     3>&1 1>&2 2>&3)
 
     case $driver_options in
-        "NVIDIA Driver Version 560 [Latest Driver Currently]")
+        "NVIDIA Driver Version 570 [Latest Driver Currently]")
+            download_and_install "570"
+            ;;
+        "NVIDIA Driver Version 560")
             download_and_install "560"
             ;;
         "NVIDIA Driver Version 550")
