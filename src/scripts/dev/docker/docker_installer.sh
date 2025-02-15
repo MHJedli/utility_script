5fd5b2fdccf4444116289c294f976eb9564640da
MHJedli <mouadjadli10@gmail.com>
2025-02-15 13:33:23 +0100

    port: docker scripts to fedora or fedora based distros

--- src/scripts/dev/docker/docker_installer.sh
+++ src/scripts/dev/docker/docker_installer.sh
@@ -7,13 +7,7 @@ trap 'handle_error "An unexpected error occurred."' ERR
 clear
 echo "Continue script execution in Docker Installation at $(date)" >> "$LOG_FILE"
 
-printc "YELLOW" "-> Checking for Internet Connection..."
-
-if check_internet; then
-
-    log_message "INFO" "Internet Connection Detected. Proceeding with Docker Installation"
-    printc "GREEN" "-> Internet Connection Detected. Proceeding with Docker Installation..."
-
+install_for_ubuntu_or_based(){
     log_message "INFO" "Refreshing Package Cache"
     printc "YELLOW" "-> Refreshing Package Cache..."
     sudo apt update || handle_error "Failed to Refresh Package Cache"
@@ -41,14 +35,66 @@ if check_internet; then
     log_message "INFO" "Verifying Installation"
     printc "YELLOW" "-> Verifying Installation..."
     sudo docker run hello-world || handle_error "Failed to Verify Docker"
+}
 
+install_for_fedora_or_based(){
+    log_message "INFO" "Refreshing Package Cache"
+    printc "YELLOW" "-> Refreshing Package Cache..."
+    sudo dnf check-update || handle_error "Failed to Refresh Package Cache"
+
+    log_message "INFO" "Installing Required Packages"
+    printc "YELLOW" "-> Installing Required Packages..."
+    sudo dnf install dnf-plugins-core || handle_error "Failed to Install Required Packages"
+    
+    log_message "INFO" "Adding Docker's Official Repository"
+    printc "YELLOW" "-> Adding Docker's Official Repository..."
+    sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo || handle_error "Failed to Add Docker's Official Repository"
+
+    log_message "INFO" "Installing Docker"
+    printc "YELLOW" "-> Installing Docker..."
+    sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y || handle_error "Failed to Install Docker"
+
+    log_message "INFO" "Starting Docker Service"
+    printc "YELLOW" "-> Starting Docker Service..."
+    sudo systemctl enable --now docker || handle_error "Failed to Start Docker Service"
+
+    log_message "INFO" "Verifying Installation"
+    printc "YELLOW" "-> Verifying Installation..."
+    sudo docker run hello-world || handle_error "Failed to Verify Docker"
+}
+
+rootless_docker(){
+    log_message "INFO" "Setting Up Rootless Docker"
+    printc "YELLOW" "-> Setting Up Rootless Docker..."
+    sudo groupadd docker
+    sudo usermod -aG docker $USER || handle_error "Failed to Set Up Rootless Docker"
+    newgrp docker
+
+    log_message "INFO" "Verifying Rootless Docker"
+    printc "YELLOW" "-> Verifying Rootless Docker..."
+    docker run hello-world || handle_error "Failed to Verify Rootless Docker"
+}
+
+# Begin Docker Installation
+printc "GREEN" "Installing for ${DISTRIBUTION_NAME}..."
+printc "YELLOW" "-> Checking for Internet Connection..."
+
+if check_internet; then
+    log_message "INFO" "Internet Connection Detected. Proceeding with Docker Installation"
+    printc "GREEN" "-> Internet Connection Detected. Proceeding with Docker Installation..."
+
+    if [[ "$DISTRIBUTION" == "ubuntu" || -n $UBUNTU_BASE ]]; then install_for_ubuntu_or_based
+    elif [[ "$DISTRIBUTION" == "fedora" || -n $FEDORA_BASE ]]; then install_for_fedora_or_based
+    else handle_error "Unsupported Distribution: $DISTRIBUTION"
+    fi
+
+    rootless_docker
+    
     echo "Script Execution in Docker Installation Ended Successfully at $(date)" >> "$LOG_FILE"
-    echo -e "${GREEN}Docker Installed Successfully...${RESET}"
     printc "GREEN" "-> Docker Installed Successfully..."
     echo "Press [ENTER] To Exit..."
     read
 else
-
     handle_error "No Internet Connection Available, Exiting..."
-
 fi
+# End Docker Installation
--- src/scripts/dev/docker/docker_remover.sh
+++ src/scripts/dev/docker/docker_remover.sh
@@ -7,6 +7,28 @@ trap 'handle_error "An unexpected error occurred."' ERR
 clear
 echo "Continue script execution in Docker Removing at $(date)" >> "$LOG_FILE"
 
+remove_for_ubuntu_or_based(){
+    log_message "INFO" "Removing Docker"
+    printc "YELLOW" "-> Removing Docker..."
+    sudo apt purge docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras -y || handle_error "Failed to remove Docker"
+
+    log_message "INFO" "Removing Leftovers"
+    printc "YELLOW" "-> Removing Leftovers..."
+    sudo rm -rf /var/lib/docker
+    sudo rm -rf /var/lib/containerd
+}
+
+remove_for_fedora_or_based(){
+    log_message "INFO" "Removing Docker"
+    printc "YELLOW" "-> Removing Docker..."
+    sudo dnf remove docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras -y || handle_error "Failed to remove Docker"
+
+    log_message "INFO" "Removing Leftovers"
+    printc "YELLOW" "-> Removing Leftovers..."
+    sudo rm -rf /var/lib/docker
+    sudo rm -rf /var/lib/containerd
+}
+
 log_message "INFO" "Checking for Docker Before Removing"
 printc "YELLOW" "-> Checking for Docker Before Removing..."
 
@@ -24,14 +46,15 @@ else
     echo -n "Press [ENTER] To Continue..."
     read
 
-    log_message "INFO" "Removing Docker"
-    printc "YELLOW" "-> Removing Docker..."
-    sudo apt purge docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras -y || handle_error "Failed to remove Docker"
-
-    log_message "INFO" "Removing Leftovers"
-    printc "YELLOW" "-> Removing Leftovers..."
-    sudo rm -rf /var/lib/docker
-    sudo rm -rf /var/lib/containerd
+    log_message "INFO" "Removing Docker for ${DISTRIBUTION_NAME}"
+    printc "YELLOW" "-> Removing Docker for ${DISTRIBUTION_NAME}..."
+    if [[ "$DISTRIBUTION" == "ubuntu" || -n "$UBUNTU_BASE" ]]; then
+        remove_for_ubuntu_or_based
+    elif [[ "$DISTRIBUTION" == "fedora" || -n "$FEDORA_BASE" ]]; then
+        remove_for_fedora_or_based
+    else
+        handle_error "Unsupported distribution: ${DISTRIBUTION}"
+    fi
 
     echo "Docker Remover Script Execution Completed Successfully at $(date)" >> "$LOG_FILE"
     printc "GREEN" "-> Docker Removed Successfully..."
