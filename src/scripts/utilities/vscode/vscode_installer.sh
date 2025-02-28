0b935de1a67cc0827ae1ccb634f64877c925e148
MHJedli <mouadjadli10@gmail.com>
2025-02-28 13:16:15 +0100

    port: vscode scripts to fedora or fedora based distributions

--- src/scripts/utilities/vscode/vscode_installer.sh
+++ src/scripts/utilities/vscode/vscode_installer.sh
@@ -7,25 +7,7 @@ trap 'handle_error "An unexpected error occurred."' ERR
 clear
 echo "Continue script execution in VS Code Installation at $(date)" >> "$LOG_FILE"
 
-log_message "INFO" "Checking for Internet Connection"
-printc "YELLOW" "-> Checking for Internet Connection..."
-
-if check_internet; then
-
-    log_message "INFO" "Internet Connection Detected. Proceeding with VS Code Installation"
-    printc "GREEN" "-> Internet Connection Detected. Proceeding with VS Code Installation..."
-
-    log_message "INFO" "Refreshing Package Cache"
-    printc "YELLOW" "-> Refreshing Package Cache..."
-    sudo apt update || handle_error "Failed to Refresh Package Cache"
-
-    log_message "INFO" "Installing the following packages: jq, curl"
-    printc "YELLOW" "-> Installing the following packages: jq, curl..."
-    sudo apt install -y curl jq || handle_error "Failed to Install Required Packages"
-
-    log_message "INFO" "Fetching The Latest VS Code Version"
-    printc "YELLOW" "-> Fetching The Latest VS Code Version..."
-    LATEST_VSCODE_VERSION=$(curl -s https://code.visualstudio.com/sha/) || handle_error "Failed to Fetch Latest VS Code Version"
+download_and_install_for_ubuntu_or_based(){
 
     log_message "INFO" "Downloading VS Code"
     printc "YELLOW" "-> Downloading VS Code..."
@@ -36,6 +18,53 @@ if check_internet; then
     log_message "INFO" "Installing VS Code"
     printc "YELLOW" "-> Installing VS Code..."
     sudo dpkg -i $DOWNLOAD_PATH/code_*.deb || handle_error "Failed To Install VS Code"
+}
+
+download_and_install_for_fedora_or_based(){
+
+    log_message "INFO" "Downloading VS Code"
+    printc "YELLOW" "-> Downloading VS Code..."
+    DOWNLOAD_LINK=$(echo $LATEST_VSCODE_VERSION | jq -r '.products[] | select(.platform.os=="linux-rpm-x64" and .build == "stable") | .url')
+    DOWNLOAD_PATH=$(pwd)/tmp/ 
+    wget -c -P $DOWNLOAD_PATH $DOWNLOAD_LINK || handle_error "Failed to Download VS Code"
+
+    log_message "INFO" "Installing VS Code"
+    printc "YELLOW" "-> Installing VS Code..."
+    sudo dnf install $DOWNLOAD_PATH/code*.rpm -y || handle_error "Failed To Install VS Code"
+}
+
+printc "GREEN" "Installing for ${DISTRIBUTION_NAME}..."
+log_message "INFO" "Checking for Internet Connection"
+printc "YELLOW" "-> Checking for Internet Connection..."
+
+if check_internet; then
+
+    log_message "INFO" "Internet Connection Detected. Proceeding with VS Code Installation"
+    printc "GREEN" "-> Internet Connection Detected. Proceeding with VS Code Installation..."
+
+    verify_packages "jq" "curl"
+
+    log_message "INFO" "Fetching The Latest VS Code Version"
+    printc "YELLOW" "-> Fetching The Latest VS Code Version..."
+    LATEST_VSCODE_VERSION=$(curl -s https://code.visualstudio.com/sha/) || handle_error "Failed to Fetch Latest VS Code Version"
+
+    if [[ "$DISTRIBUTION" == "ubuntu" || -n "$UBUNTU_BASED" ]]; then
+
+        log_message "INFO" "Refreshing Package Cache"
+        printc "YELLOW" "-> Refreshing Package Cache..."
+        sudo apt update || handle_error "Failed to Refresh Package Cache"
+        download_and_install_for_ubuntu_or_based
+
+    elif [[ "$DISTRIBUTION" == "fedora" || -n "$FEDORA_BASED" ]]; then
+
+        log_message "INFO" "Refreshing Package Cache"
+        printc "YELLOW" "-> Refreshing Package Cache..."
+        sudo dnf update -y || handle_error "Failed to Refresh Package Cache"
+        download_and_install_for_fedora_or_based
+
+    else
+        handle_error "Unsupported OS : ${DISTRIBUTION_NAME}. Exiting..."
+    fi
 
     echo "VS Code Installer Script Completed Successfully at $(date)" >> "$LOG_FILE"
     print_msgbox "Success !" "VS Code Installed Successfully"
--- src/scripts/utilities/vscode/vscode_remover.sh
+++ src/scripts/utilities/vscode/vscode_remover.sh
@@ -7,6 +7,20 @@ trap 'handle_error "An unexpected error occurred."' ERR
 clear
 echo "Continue script execution in VS Code Removal at $(date)" >> "$LOG_FILE"
 
+remove_for_ubuntu_or_based(){
+
+    log_message "INFO" "Removing VS Code"
+    printc "YELLOW" "-> Removing VS Code..."
+    sudo apt autoremove --purge code -y || handle_error "Failed to remove VS Code"
+}
+
+remove_for_fedora_or_based(){
+
+    log_message "INFO" "Removing VS Code for Fedora"
+    printc "YELLOW" "-> Removing VS Code for Fedora..."
+    sudo dnf remove code -y || handle_error "Failed to remove VS Code for Fedora"
+}
+
 log_message "INFO" "Checking for VS Code Before Removing"
 printc "YELLOW" "-> Checking for VS Code Before Removing..."
 
@@ -23,9 +37,19 @@ else
     printc "GREEN" "-> VS Code is installed."
     echo -n "Press [ENTER] To Continue..."
     
-    log_message "INFO" "Removing VS Code"
-    printc "YELLOW" "-> Removing VS Code..."
-    sudo apt autoremove --purge code -y || handle_error "Failed to remove VS Code"
+    if [[ "$DISTRIBUTION" == "ubuntu" || -n "$UBUNTU_BASED" ]]; then
+
+        remove_for_ubuntu_or_based
+
+    elif [[ "$DISTRIBUTION" == "fedora" || -n "$FEDORA_BASED" ]]; then
+
+        remove_for_fedora_or_based
+
+    else
+
+        handle_error "Unsupported OS : ${DISTRIBUTION_NAME}. Exiting..."
+
+    fi
 
     echo "VS Code Remover Script Execution Completed Successfully at $(date)" >> "$LOG_FILE"
     print_msgbox "Success" "VS Code Removed Successfully"
